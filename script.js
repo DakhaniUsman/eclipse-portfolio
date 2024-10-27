7e79efd97342e695ad0c759ac146278158ed2262
Dakhani Usman <115381340+DakhaniUsman@users.noreply.github.com>
2024-10-28 00:44:56 +0530

    updates

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+
+
+function hide() {
+    const sidebar = document.querySelector('.offScreenMenu');
+    sidebar.style.display.toggle = 'none';
+    hamMenu.classList.toggle("active");
+    offScreenMenu.classList.toggle("active");
+}
+
 const hamMenu = document.querySelector(".hamMenu");
 
 const offScreenMenu = document.querySelector(".offScreenMenu");
